0d3a7dec510066828779bef5459d9f6d9dac969e
pavelbesedin <pavelbesedin97@mail.ru>
2023-05-08 20:17:13 +0300

    ArrayList done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        ## Задание 1
+//
+//Напишите код, с помощью которого можно напечатать только нечетные числа в консоль.
+// Код должен работать с любой последовательностью и объемом списка чисел.
+//В качестве отладочной информации можете использовать список чисел
+
+        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+
+        for (Integer num : nums) {
+            if (num % 2 == 0) {
+                System.out.println(num);
+            }
+        }
     }
 }
